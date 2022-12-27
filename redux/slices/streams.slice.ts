b54306330240601a217aae35f41f5498861d3601
nitegeist <nitegeist.dev@gmail.com>
2022-12-26 23:14:17 -0500

    use RTK Query instead of CreateAsyncThunk

---
+++ redux/slices/streams.slice.ts
@@ -0,0 +1,70 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/dist/query/react';
+import { startFlow } from '@richochet/api/ethereum';
+import { transformError } from '@richochet/utils/transformError';
+import { getContract } from '@wagmi/core';
+import { idaABI } from 'constants/abis';
+import { idaAddress } from 'constants/polygon_config';
+
+export interface Payload {
+	amount: string;
+	config: {
+		[key: string]: string;
+	};
+	callback: (e?: string) => void;
+}
+
+const streamApi = createApi({
+	keepUnusedDataFor: 60, // 60 seconds (default)
+	reducerPath: 'streams',
+	baseQuery: fetchBaseQuery(),
+	endpoints: (builder) => ({
+		startStream: builder.query<Payload | null, string>({
+			//Payload type:
+			//config.superToken,
+			//config.tokenA,
+			//config.tokenB,
+			//normalizedAmount,
+			//config.referralId
+
+			queryFn: async (payload: any): Promise<any | undefined> => {
+				try {
+					const { config, amount } = payload;
+					// we must initialize a contract address with idaContract: getContract(idaAddress, idaABI, web3);
+					const idaContract = await getContract({ address: idaAddress, abi: idaABI });
+					console.log(idaContract);
+					// We must normalize the payload amount for superfluid function
+					const normalizedAmount = Math.round((Number(amount) * 1e18) / 2592000);
+					// we must call the startFlow function in api/ethereum.ts with following Data
+					//
+					//idaContract: Initialized Contract Object from line 18 comment,
+					//exchangeAddress: string, (part of config) from user input
+					//inputTokenAddress: string,  (part of config) from user input
+					//outputTokenAddress: string, (part of config) from user input
+					//amount: number, // (part of config) from user input
+					//web3: Web3,  //Initialized in line 17, WEB3 object
+					//referralId?: string, //this comes from state
+					const streamTx = await startFlow(
+						idaContract,
+						config.superToken,
+						config.tokenA,
+						config.tokenB,
+						normalizedAmount,
+						config.referralId
+					);
+					console.log(streamTx);
+					return {
+						data: streamTx,
+					};
+					// payload.callback();
+				} catch (e) {
+					console.error(e);
+					const error = transformError(e);
+					return { error };
+					// payload.callback(error);
+				}
+			},
+		}),
+	}),
+});
+
+export default streamApi;
--- redux/store.ts
+++ redux/store.ts
@@ -2,18 +2,24 @@ import { configureStore } from '@reduxjs/toolkit';
 import {
 	allSubgraphEndpoints,
 	createApiWithReactHooks,
+	initializeRpcApiSlice,
 	initializeSubgraphApiSlice
 } from '@superfluid-finance/sdk-redux';
-import { streamsSlice } from './slices/streamsReducer.slice';
+import { balanceRpcApiEndpoints } from './balanceRpcApiEndpoints';
+import streamApi from './slices/streams.slice';
 
 export const sfSubgraph = initializeSubgraphApiSlice(createApiWithReactHooks).injectEndpoints(allSubgraphEndpoints);
+export const rpcApi = initializeRpcApiSlice(createApiWithReactHooks).injectEndpoints(balanceRpcApiEndpoints);
 
 export const store = configureStore({
 	reducer: {
-		[streamsSlice.name]: streamsSlice.reducer,
+		[rpcApi.reducerPath]: rpcApi.reducer,
+		[streamApi.reducerPath]: streamApi.reducer,
+		// [streams.name]: streams.reducer,
 		[sfSubgraph.reducerPath]: sfSubgraph.reducer,
 	},
-	// middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(streamsApi.middleware),
+	middleware: (getDefaultMiddleware) =>
+		getDefaultMiddleware().concat(streamApi.middleware).concat(rpcApi.middleware).concat(sfSubgraph.middleware),
 });
 
 export type AppState = ReturnType<typeof store.getState>;
