b74e8d783df4faa27696422a8f6a4053204277d4
nitegeist <nitegeist.dev@gmail.com>
2022-12-28 16:48:31 -0500

    Refactor store

--- redux/store.ts
+++ redux/store.ts
@@ -1,4 +1,4 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore, PreloadedState, StateFromReducersMapObject } from '@reduxjs/toolkit';
 import {
 	allSubgraphEndpoints,
 	createApiWithReactHooks,
@@ -12,21 +12,28 @@ import streamApi from './slices/streams.slice';
 export const sfSubgraph = initializeSubgraphApiSlice(createApiWithReactHooks).injectEndpoints(allSubgraphEndpoints);
 export const rpcApi = initializeRpcApiSlice(createApiWithReactHooks).injectEndpoints(balanceRpcApiEndpoints);
 
-export const store = configureStore({
-	reducer: {
-		[rpcApi.reducerPath]: rpcApi.reducer,
-		[streamApi.reducerPath]: streamApi.reducer,
-		[coingeckoApi.reducerPath]: coingeckoApi.reducer,
-		// [streams.name]: streams.reducer,
-		[sfSubgraph.reducerPath]: sfSubgraph.reducer,
-	},
-	middleware: (getDefaultMiddleware) =>
-		getDefaultMiddleware()
-			.concat(streamApi.middleware)
-			.concat(coingeckoApi.middleware)
-			.concat(rpcApi.middleware)
-			.concat(sfSubgraph.middleware),
-});
+const reducer = {
+	[rpcApi.reducerPath]: rpcApi.reducer,
+	[streamApi.reducerPath]: streamApi.reducer,
+	[coingeckoApi.reducerPath]: coingeckoApi.reducer,
+	// [streams.name]: streams.reducer,
+	[sfSubgraph.reducerPath]: sfSubgraph.reducer,
+};
 
-export type AppState = ReturnType<typeof store.getState>;
-export type AppDispatch = typeof store.dispatch;
+export type AppState = StateFromReducersMapObject<typeof reducer>;
+
+export const store = (preloadedState?: PreloadedState<AppState>) =>
+	configureStore({
+		reducer: reducer,
+		middleware: (getDefaultMiddleware) =>
+			getDefaultMiddleware()
+				.concat(streamApi.middleware)
+				.concat(coingeckoApi.middleware)
+				.concat(rpcApi.middleware)
+				.concat(sfSubgraph.middleware),
+		preloadedState,
+	});
+
+type Store = ReturnType<typeof store>;
+
+export type AppDispatch = Store['dispatch'];
