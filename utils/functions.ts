08698bb66b33b75144af88b1acacbd1405abd5b1
nitegeist <nitegeist.dev@gmail.com>
2022-12-31 18:14:37 -0500

    Change functions.ts to helperFunctions.ts

--- utils/functions.ts
+++
@@ -1,7 +0,0 @@
-export const combineClasses = (...classes: string[]) => classes.filter(Boolean).join(' ');
-export const formatCurrency = (number: number, currency: string = 'USD') =>
-	new Intl.NumberFormat('en-US', {
-		style: 'currency',
-		currency: currency,
-		useGrouping: true,
-	}).format(number);
